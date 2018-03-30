724e281430c4f819248dd658e7080f042c688a2e
connor ingold <connoringold@gmail.com>
2018-03-30 22:15:57 +0100

    removed JS which was causing heroku problems

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,3 @@ console.log('Hello World from Webpacker')
 
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
-
-import { scrollDown } from '../components/button';
-scrollDown
